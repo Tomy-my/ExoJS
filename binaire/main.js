ffdbf92b5f4d1ba51e23cc578e722ba444d17568
Tomy-my <azerouto@gmail.com>
2021-05-21 11:50:41 +0200

    Text to Binary

---
+++ binaire/main.js
@@ -0,0 +1,11 @@
+const convert = document.getElementById("convert") ;
+convert.addEventListener("click", function() {
+
+    let code =document.getElementById("msgToCode").value;
+    let crypted =document.getElementById("crypted");
+    crypted.value = "";
+    for (i=0; i < code.length; i++) {
+     	crypted.value +=code[i].charCodeAt(0).toString(2) + " ";
+    }
+    document.getElementById("crypted").textContent = crypted.value;
+});
