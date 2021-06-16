017e4b8b3fef214c9aca1d8642cf17e11af96b61
Tomy-my <azerouto@gmail.com>
2021-06-16 09:02:32 +0200

    *

--- navbar/main.js
+++ navbar/main.js
@@ -1,4 +1,9 @@
 window.onscroll = slideDown;
 function slideDown() {
-    // Votre code ici
+    
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("navbar").style.top = "0";
+    } else {
+        document.getElementById("navbar").style.top = "-50px";
+    }
 }
